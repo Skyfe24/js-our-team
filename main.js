4178af8903772badb709980403a7e35ed532f35b
Skyfe24 <fabio.erase@gmail.com>
2023-06-12 16:50:11 +0200

    Objects Array created (Milestone1 completed)

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
-console.log('JS Ok')
-
-
-
-
-
-
-
-
-
-
+console.log("JS Ok");
+// creo il mio array del team usando i dati che ci sono stati forniti
+const ourTeam = [
+  {
+    memberName: "Wayne Barnett",
+    role: "Founder & CEO ",
+    memberImage: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    memberName: "Angela Caroll",
+    role: "Chief Editor ",
+    memberImage: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    memberName: "Walter Gordon",
+    role: "Office Manager",
+    memberImage: "walter-gordon-office-manager.jpg",
+  },
+  {
+    memberName: "Angela Lopez",
+    role: "Social Media Manager",
+    memberImage: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    memberName: "Scott Estrada",
+    role: "Developer ",
+    memberImage: "scott-estrada-developer.jpg",
+  },
+  {
+    memberName: "Barbara Ramos",
+    role: "Graphic Designer  ",
+    memberImage: "barbara-ramos-graphic-designer.jpg",
+  },
+];
 
 // Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
 // Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
